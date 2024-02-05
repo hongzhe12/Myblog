775a19ae6e4692f5792b92a1de174d18b0029d95
huang-hongzhe <hongzhe2022@163.com>
2024-02-05 08:37:46 +0800

    first commit

---
+++ comment/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from article.models import ArticlePost
+
+# django-ckeditor
+from ckeditor.fields import RichTextField
+# django-mptt
+from mptt.models import MPTTModel, TreeForeignKey
+
+# 博文的评论
+class Comment(MPTTModel):
+    # 新增，mptt树形结构
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+    # 新增，记录二级评论回复给谁, str
+    reply_to = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replyers'
+    )
+
+
+    article = models.ForeignKey(
+        ArticlePost,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    body = RichTextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     ordering = ('created',)
+
+    # 替换 Meta 为 MPTTMeta
+    class MPTTMeta:
+        order_insertion_by = ['created']
+
+    def __str__(self):
+        return self.body[:20]
